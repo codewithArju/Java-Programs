f36ec68d3a05377a098541cc2bbba6acddd5f33f
codewithArju <arjumallick289@gmail.com>
2026-06-09 20:25:49 +0530

    Added ValidTriangle program 9/6/2026

---
+++ Level2 Java/ValidTriangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ValidTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter First Value");
+        float a = sc.nextFloat();
+
+        System.out.println("Enter Second Value");
+        float b = sc.nextFloat();
+
+        System.out.println("Enter Third Value");
+        float c = sc.nextFloat();
+
+        if ((a + b > c) && (a + c > b) && (b + c > a)) {
+            System.out.println("Valid Triangle");
+        } else {
+            System.out.println("Invalid Triangle");
+        }
+
+        sc.close();
+    }
+}
